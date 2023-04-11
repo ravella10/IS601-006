cdda3ce4b53c5dc8f7e8fe4521830d7e90e39666
levi45 <dantevale45@gmail.com>
2023-04-11 18:15:49 -0400

    final1

--- BusinessManagement/views/forms.py
+++ BusinessManagement/views/forms.py
@@ -3,14 +3,14 @@ from wtforms import EmailField, SubmitField, StringField
 from wtforms.validators import DataRequired, Email, InputRequired, Length, regexp
 
 class CompanyForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired(),Length(1,60)])
-    address = StringField("Address", validators=[InputRequired(),Length(1,100)])
-    city = StringField("City", validators=[InputRequired(),Length(1,20)])
-    zip = StringField("Zip", validators=[DataRequired(), InputRequired(), Length(5)], render_kw={"pattern": '[0-9]*', "title": "Enter at least 5 digits"})
+    name = StringField("Name")
+    address = StringField("Address")
+    city = StringField("City")
+    zip = StringField("Zip")
     website = StringField("Website")
     submit = SubmitField("Add Company")
 
 class EmployeeForm(FlaskForm):
-    first_name = StringField("First Name", validators=[DataRequired(),Length(1,20)])
-    last_name = StringField("Last Name", validators=[DataRequired(),Length(1,20)])
-    email = EmailField("Email", validators=[DataRequired(), Email()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    email = EmailField("Email")
